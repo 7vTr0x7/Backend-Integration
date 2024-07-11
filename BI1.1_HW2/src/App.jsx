06c04e04a76f31ae4b81d4a533548e902227f142
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 18:16:30 +0530

    BI1.1_HW2 - hotels by title

--- BI1.1_HW2/src/App.jsx
+++ BI1.1_HW2/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hotels from "./components/Hotels";
+import HotelsByTitle from "./components/HotelsByTitle";
 
 const App = () => {
   return (
     <div>
       <Hotels />
+      <HotelsByTitle />
     </div>
   );
 };
---
+++ BI1.1_HW2/src/components/HotelsByTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HotelsByTitle = () => {
+  return <div></div>;
+};
+
+export default HotelsByTitle;
