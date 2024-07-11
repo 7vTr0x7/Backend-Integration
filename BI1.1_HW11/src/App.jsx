bf91f7f812f654e73687ed48c5927dca972a8587
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 17:23:24 +0530

    BI1.1_HW1 - Book by author

--- BI1.1_HW11/src/App.jsx
+++ BI1.1_HW11/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Books from "./components/Books";
 import BooksByTitle from "./components/BooksByTitle.jsx";
+import BooksByAuthor from "./components/BooksByAuthor.jsx";
 
 const App = () => {
   return (
     <div>
       <Books />
       <BooksByTitle title="Shoe Dog" />
+      <BooksByAuthor author="Harper Lee" />
     </div>
   );
 };
---
+++ BI1.1_HW11/src/components/BooksByAuthor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BooksByAuthor = () => {
+  return <div></div>;
+};
+
+export default BooksByAuthor;
