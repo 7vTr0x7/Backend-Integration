c435f687d0b5a738fd9c5c76706db52916fd2501
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 17:46:37 +0530

    BI1.1_HW2

--- BI1.1_HW2/src/main.jsx
+++ BI1.1_HW2/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hotels from "./components/Hotels";
 
 const main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Hotels />
+    </div>
+  );
 };
 
 export default main;
