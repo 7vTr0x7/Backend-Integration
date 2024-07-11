ae00b989f9fedef68159e20d13262b55d30aa606
7vTr0x7 <7vTrox7@gmail.com>
2024-07-11 16:35:35 +0530

    BI1.1_CW - Movies

--- BI1.1_HW1/src/components/Movies.jsx
+++ BI1.1_HW1/src/components/Movies.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const Movies = () => {
-  return <div></div>;
+  const { data, loading, error } = useFetch("http://localhost:4000/movies");
+
+  return (
+    <div>
+      <ul>
+        {data && data.map((movie) => <li key={movie._id}>{movie.title}</li>)}
+      </ul>
+    </div>
+  );
 };
 
 export default Movies;
