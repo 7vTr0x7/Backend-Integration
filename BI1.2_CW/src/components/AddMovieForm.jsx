8b5d5a918c09b8af3644fd999bf3cb7aec2df833
7vTr0x7 <7vTrox7@gmail.com>
2024-07-12 10:59:00 +0530

    BI1.2_CW - addMovieForm - form

--- BI1.2_CW/src/components/AddMovieForm.jsx
+++ BI1.2_CW/src/components/AddMovieForm.jsx
@@ -36,6 +36,99 @@ const AddMovieForm = () => {
           type="text"
           value={formData.title}
           onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="releaseYear">Release Year:</label>
+        <br />
+        <input
+          id="releaseYear"
+          type="text"
+          value={formData.releaseYear}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="genre">Genre:</label>
+        <br />
+        <input
+          id="genre"
+          type="text"
+          value={formData.genre}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="director">Director:</label>
+        <br />
+        <input
+          id="director"
+          type="text"
+          value={formData.director}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="actors">Actors:</label>
+        <br />
+        <input
+          id="actors"
+          type="text"
+          value={formData.actors}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="language">Language:</label>
+        <br />
+        <input
+          id="language"
+          type="text"
+          value={formData.language}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="country">Country:</label>
+        <br />
+        <input
+          id="country"
+          type="text"
+          value={formData.country}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="rating">Rating:</label>
+        <br />
+        <input
+          id="rating"
+          type="text"
+          value={formData.rating}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="awards">Awards:</label>
+        <br />
+        <input
+          id="awards"
+          type="text"
+          value={formData.awards}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="posterUrl">Poster URL:</label>
+        <br />
+        <input
+          id="posterUrl"
+          type="text"
+          value={formData.posterUrl}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <label htmlFor="trailerUrl">Trailer URL:</label>
+        <br />
+        <input
+          id="trailerUrl"
+          type="text"
+          value={formData.trailerUrl}
+          onChange={onChangeHandler}></input>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
