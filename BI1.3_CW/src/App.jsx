ae7ce17a339afbf67690b611ec44430e46005047
7vTr0x7 <7vTrox7@gmail.com>
2024-07-12 14:58:47 +0530

    BI1.3_CW - delete movie

--- BI1.3_CW/src/App.jsx
+++ BI1.3_CW/src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Movies from "./components/Movies";
 import MovieByTitle from "./components/MovieByTitle";
 import AddMovieForm from "./components/AddMovieForm";
-import "./App.css";
 
 const App = () => {
   return (
