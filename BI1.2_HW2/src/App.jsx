607a81d13791f7c9c65306319ab808ca195ab5b2
7vTr0x7 <7vTrox7@gmail.com>
2024-07-12 12:59:57 +0530

    BI1.2_HW2 - AddHotelForm

--- BI1.2_HW2/src/App.jsx
+++ BI1.2_HW2/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hotels from "./components/Hotels";
 import HotelsByTitle from "./components/HotelsByTitle";
+import AddHotelForm from "./components/AddHotelForm";
 
 const App = () => {
   return (
     <div>
+      <AddHotelForm />
       <Hotels />
       <HotelsByTitle name="New Hotel 1" />
     </div>
