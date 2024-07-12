5df2be5c45560bbd535cd5e70434cf9583d31052
7vTr0x7 <7vTrox7@gmail.com>
2024-07-12 10:38:27 +0530

    BI1.2_CW - addMovieForm

--- BI1.2_CW/src/App.jsx
+++ BI1.2_CW/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Movies from "./components/Movies";
 import MovieByTitle from "./components/MovieByTitle";
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = () => {
   return (
     <div>
+      <AddMovieForm />
       <Movies />
       <MovieByTitle title="Gully Boy" />
     </div>
