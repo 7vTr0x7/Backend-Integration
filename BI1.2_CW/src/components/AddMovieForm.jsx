bc1d34cdd1e77b1abcbb5688fe82b259b8431770
7vTr0x7 <7vTrox7@gmail.com>
2024-07-12 10:51:06 +0530

    BI1.2_CW - addMovieForm

--- BI1.2_CW/src/components/AddMovieForm.jsx
+++ BI1.2_CW/src/components/AddMovieForm.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddMovieForm = () => {
-  return <div></div>;
+  const [formData, setFormData] = useState({
+    title: "",
+    releaseYear: "",
+    genre: "",
+    director: "",
+    actors: "",
+    language: "",
+    country: "",
+    rating: "",
+    plot: "",
+    awards: "",
+    posterUrl: "",
+    trailerUrl: "",
+  });
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]:
+        name === "releaseYear" || name === "rating" ? parseInt(value) : value,
+    }));
+  };
+
+  return (
+    <div>
+      <h2>Add New Movie</h2>
+      <form>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          id="title"
+          type="text"
+          value={formData.title}
+          onChange={onChangeHandler}></input>
+      </form>
+    </div>
+  );
 };
 
 export default AddMovieForm;
